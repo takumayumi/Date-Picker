586c8011238095a8071c8e7d3e815f9adcc08e03
Yumi Takuma <yumitakuma@icloud.com>
2024-07-18 04:14:43 +0800

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
           "style-loader",
           "css-loader",
           "postcss-loader",
-          MiniCssExtractPlugin.loader,
+          // MiniCssExtractPlugin.loader,
         ],
       },
       {
