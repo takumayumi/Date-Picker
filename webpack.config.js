952a58d1dfe223bf32c677eaf734b60abb49c39b
Yumi Takuma <yumitakuma@icloud.com>
2024-07-18 06:17:43 +0800

    Added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "index.html",
+      favicon: "./public/favicon.ico",
     }),
     new MiniCssExtractPlugin(),
     new CompressionPlugin({
